1977cb3b21fd2528204f6e1a988ad978a820fbbd
Anton <levantonio@mail.ru>
2022-11-16 17:05:34 +0300

    task 13

---
+++ homework-task13/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №13.
+// Написать программу, которая:
+// 1. выводит 3 цифру заданного числа
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int ThirdDigit(int number)
+{
+    int result = -1; // на семинаре говорили, что можго использовать -1 если числа нет
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+        result = number % 10;
+    }
+    return result;
+
+} 
+int result = ThirdDigit(number);
+if(result == -1) Console.WriteLine($"{number} --> двузначное число");
+else Console.WriteLine($"{number} -- > {result}");
+
