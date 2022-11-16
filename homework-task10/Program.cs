e514d136a0d215c62d6d56ebd22f30ed278c4470
Anton <levantonio@mail.ru>
2022-11-16 14:47:57 +0300

    task 10

---
+++ homework-task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №10
+// Написать программу, которая:
+// 1. Принимает на ввод трёхзначное число
+// 2. На выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int AverageNumber(int num)
+{
+    int figure = (num / 10) % 10;
+    return figure;
+}
+
+int result = AverageNumber(number);
+if (number > 99 && number < 999)
+{
+    Console.WriteLine($"Средняя цифра числа: {number} --> {result}");
+}
+else
+    Console.WriteLine($"{number}-Число не трехзначное");
